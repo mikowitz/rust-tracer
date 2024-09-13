16cec86c33b84ef54933d399fda9b67a74e1d6ee
Michael Berkowitz <michael.berkowitz@gmail.com>
2024-09-12 23:44:54 -0400

    12. Positionable Camera

--- src/camera.rs
+++ src/camera.rs
@@ -1,7 +1,7 @@
 use core::f32;
 use indicatif::{ProgressBar, ProgressStyle};
 use rand::prelude::*;
-use std::{fs::File, io::Write};
+use std::{f32::consts::PI, fs::File, io::Write};
 
 use crate::{
     color::Color,
@@ -18,6 +18,10 @@ pub struct Camera {
     pub aspect_ratio: f32,
     pub samples_per_pixel: u32,
     pub max_depth: u32,
+    pub vfov: f32,
+    pub lookfrom: Point,
+    pub lookat: Point,
+    pub vup: Vector,
 
     image_height: u32,
     center: Point,
@@ -89,20 +93,26 @@ impl Camera {
 
         self.pixels_sample_scale = 1.0 / (self.samples_per_pixel as f32);
 
-        self.center = Point::new(0., 0., 0.);
+        self.center = self.lookfrom;
 
-        let focal_length = 1.0;
-        let viewport_height = 2.0;
+        let focal_length = (self.lookfrom - self.lookat).magnitude();
+        let θ = degrees_to_radians(self.vfov);
+        let h = (θ / 2.0).tan();
+        let viewport_height = 2.0 * h * focal_length;
         let viewport_width = viewport_height * (self.image_width as f32 / self.image_height as f32);
 
-        let viewport_u = Vector::new(viewport_width, 0., 0.);
-        let viewport_v = Vector::new(0., -viewport_height, 0.);
+        let w = (self.lookfrom - self.lookat).normalize();
+        let u = self.vup.cross(w).normalize();
+        let v = w.cross(u);
+
+        let viewport_u = u * viewport_width;
+        let viewport_v = -v * viewport_height;
 
         self.pixelδu = viewport_u / self.image_width as f32;
         self.pixelδv = viewport_v / self.image_height as f32;
 
         let viewport_upper_left =
-            self.center - Vector::new(0., 0., focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+            self.center - w * focal_length - viewport_u / 2.0 - viewport_v / 2.0;
 
         self.pixel00_loc = viewport_upper_left + (self.pixelδu + self.pixelδv) * 0.5;
     }
@@ -123,4 +133,8 @@ fn ray_color(ray: &Ray, depth: u32, world: &World) -> Color {
     let unit_direction = ray.direction.normalize();
     let a = 0.5 * (unit_direction.y + 1.0);
     Color::white() * (1.0 - a) + Color::new(0.5, 0.7, 1.0) * a
+}
+
+fn degrees_to_radians(degrees: f32) -> f32 {
+    degrees * PI / 180.0
 }
