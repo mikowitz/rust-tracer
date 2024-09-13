7ff5bb213995c71fd1a390da543f832d9b7e66e0
Michael Berkowitz <michael.berkowitz@gmail.com>
2024-09-12 20:33:08 -0400

    9. Diffuse Materials (#9)

--- src/color.rs
+++ src/color.rs
@@ -13,10 +13,17 @@ impl Color {
 
     pub fn to_ppm(self) -> String {
         let i = Interval::new(0.000, 0.999);
-        let r = (256.0 * i.clamp(self.x)) as u32;
-        let g = (256.0 * i.clamp(self.y)) as u32;
-        let b = (256.0 * i.clamp(self.z)) as u32;
+        let r = (256.0 * i.clamp(linear_to_gamma(self.x))) as u32;
+        let g = (256.0 * i.clamp(linear_to_gamma(self.y))) as u32;
+        let b = (256.0 * i.clamp(linear_to_gamma(self.z))) as u32;
 
         format!("{} {} {}", r, g, b)
     }
+}
+
+fn linear_to_gamma(linear: f32) -> f32 {
+    if linear > 0.0 {
+        return linear.sqrt();
+    }
+    0.0
 }
